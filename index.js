90c5548a22eadcac879e6bb072a4945ce007bcda
Zenrer <ram_idts@me.com>
2022-06-12 09:45:20 +1000

    finished part 3 exercise 9

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const cors = require("cors");
 const express = require("express");
 const morgan = require("morgan");
 
@@ -5,6 +6,7 @@ const app = express();
 
 morgan.token("body", (request, response) => JSON.stringify(request.body));
 
+app.use(cors());
 app.use(express.json());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
