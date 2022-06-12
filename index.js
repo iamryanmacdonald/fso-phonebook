f75250bac32ec8ad3076dbcfbea2013e223eead1
Zenrer <ram_idts@me.com>
2022-06-12 22:13:41 +1000

    finished part 3 exercise 17

--- index.js
+++ index.js
@@ -18,29 +18,6 @@ app.use(
 
 app.use(express.static("build"));
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.delete("/api/persons/:id", (request, response, next) => {
   const id = request.params.id;
 
@@ -93,6 +70,28 @@ app.post("/api/persons", (request, response, next) => {
   person
     .save()
     .then((person) => response.json(person))
+    .catch((error) => next(error));
+});
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const id = request.params.id;
+  const body = request.body;
+
+  if (!body.number) {
+    return response.status(400).json({ error: "number missing" });
+  }
+
+  Person.findById(id)
+    .then((person) => {
+      person.number = request.body.number;
+
+      person
+        .save()
+        .then((updatedPerson) => {
+          response.json(updatedPerson);
+        })
+        .catch((error) => next(error));
+    })
     .catch((error) => next(error));
 });
 
