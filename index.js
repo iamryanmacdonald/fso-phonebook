dff093f977b3ff624062e52e1192c9bceee27d29
Zenrer <ram_idts@me.com>
2022-06-12 18:00:29 +1000

    finished part 3 exercise 16

--- index.js
+++ index.js
@@ -41,26 +41,32 @@ let persons = [
   },
 ];
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
   const id = request.params.id;
 
-  Person.findByIdAndDelete(id).then(() => response.status(204).end());
+  Person.findByIdAndDelete(id)
+    .then(() => response.status(204).end())
+    .catch((error) => next(error));
 });
 
-app.get("/api/persons", (request, response) => {
-  Person.find({}).then((persons) => response.json(persons));
+app.get("/api/persons", (request, response, next) => {
+  Person.find({})
+    .then((persons) => response.json(persons))
+    .catch((error) => next(error));
 });
 
 app.get("/api/persons/:id", (request, response, next) => {
   const id = request.params.id;
 
-  Person.findById(id).then((person) => {
-    if (person) {
-      response.json(person);
-    } else {
-      response.status(404).end();
-    }
-  });
+  Person.findById(id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 app.get("/info", (request, response) => {
@@ -71,7 +77,7 @@ app.get("/info", (request, response) => {
   `);
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
 
   if (!body.name) {
@@ -84,8 +90,23 @@ app.post("/api/persons", (request, response) => {
 
   const person = new Person(body);
 
-  person.save().then((person) => response.json(person));
+  person
+    .save()
+    .then((person) => response.json(person))
+    .catch((error) => next(error));
 });
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 
