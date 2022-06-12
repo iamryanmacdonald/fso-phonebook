d12a4c887194145a2af24d5236358d15828c7695
Zenrer <ram_idts@me.com>
2022-06-12 22:31:21 +1000

    finished part 3 exercise 19

--- index.js
+++ index.js
@@ -61,14 +61,6 @@ app.get("/info", (request, response, next) => {
 app.post("/api/persons", (request, response, next) => {
   const body = request.body;
 
-  if (!body.name) {
-    return response.status(400).json({ error: "name missing" });
-  }
-
-  if (!body.number) {
-    return response.status(400).json({ error: "number missing" });
-  }
-
   const person = new Person(body);
 
   person
@@ -78,23 +70,15 @@ app.post("/api/persons", (request, response, next) => {
 });
 
 app.put("/api/persons/:id", (request, response, next) => {
-  const id = request.params.id;
-  const body = request.body;
+  const { number } = request.body;
 
-  if (!body.number) {
-    return response.status(400).json({ error: "number missing" });
-  }
-
-  Person.findById(id)
-    .then((person) => {
-      person.number = request.body.number;
-
-      person
-        .save()
-        .then((updatedPerson) => {
-          response.json(updatedPerson);
-        })
-        .catch((error) => next(error));
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { number },
+    { new: true, runValidators: true, context: "query" }
+  )
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
     })
     .catch((error) => next(error));
 });
@@ -104,6 +88,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
